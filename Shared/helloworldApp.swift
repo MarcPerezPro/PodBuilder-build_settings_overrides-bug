e5d3369282b570ae3eabe6acf644a5080540fca5
Marc PEREZ <marc.perez@epitech.eu>
2021-04-07 15:42:42 +0200

    Initial Commit

---
+++ Shared/helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloworldApp.swift
+//  Shared
+//
+//  Created by Marc PEREZ on 07/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
